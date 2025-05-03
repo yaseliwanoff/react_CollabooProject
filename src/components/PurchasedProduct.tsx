e5fd737c622dc4ca7728bd20752e0a608668eb04
Yaroslav Selivanov <144848251+yaseliwanoff@users.noreply.github.com>
2025-05-03 22:39:30 +0300

    Update PurchasedProduct.tsx

--- src/components/PurchasedProduct.tsx
+++ src/components/PurchasedProduct.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Button } from '@/components/ui/button';
 
 interface PurchasedProductProps {
-    avatar: string;
-    title: string;
-    description: string;
+  avatar: string;
+  title: string;
+  description: string;
+  date_end: string;
+  links: { href: string }[];
 }
 
-const PurchasedProduct: React.FC<PurchasedProductProps> = ({ avatar, title, description }) => {
+const PurchasedProduct: React.FC<PurchasedProductProps> = ({ avatar, title, description, date_end, links }) => {
   return (
     <div className="bg-white flex font-[Inter] flex-col w-[471px] h-[402px] rounded-[12px] border border-[#E4E4E7] py-6 px-6">
       <div className="flex flex-col flex-grow">
@@ -18,18 +20,25 @@ const PurchasedProduct: React.FC<PurchasedProductProps> = ({ avatar, title, desc
           <div className="flex mt-4 items-center gap-2">
             <h4 className="font-semibold text-[16px]">{title}</h4>
           </div>
-          <p className="font-normal text-[14px] opacity-55">{description}</p>
+          <p className="font-normal text-[14px] opacity-55 whitespace-pre-line">{description}</p>
         </div>
         <div className="w-[100%] h-[1px] bg-[#E4E4E7]"></div>
       </div>
       <div className="mt-4 flex justify-between items-center w-full">
         <div className="flex flex-col text-[14px]">
           <span className="font-semibold">Active till</span>
-          <span className="font-normal text-[#71717A]">01.01.2025</span>
+          <span className="font-normal text-[#71717A]">{date_end}</span>
         </div>
-        <div className="flex gap-3">
-          <Button variant={"default"}>Connect #1</Button>
-          <Button variant={"default"}>Connect #2</Button>
+        <div className="flex gap-3 flex-wrap">
+          {links.map((link, idx) => (
+            <Button
+              key={idx}
+              variant={"default"}
+              onClick={() => window.open(link.href, "_blank")}
+            >
+              Connect #{idx + 1}
+            </Button>
+          ))}
         </div>
       </div>
     </div>
