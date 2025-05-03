d5ae7d19d51b46cbf8b8389ba351543c0262e4a1
Yaroslav Selivanov <144848251+yaseliwanoff@users.noreply.github.com>
2025-05-03 22:15:42 +0300

    Update login-form.tsx
    created the ability to go to another page when interacting with a link

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -86,7 +86,9 @@ export function LoginForm({
         <div className="grid gap-3 font-[Inter]">
           <div className="flex items-center">
             <Label htmlFor="password">Password</Label>
-            <a href="#" className="ml-auto text-sm underline-offset-4 hover:underline">Forgot your password?</a>
+            <Link to="/forgot-password" className="ml-auto text-sm underline-offset-4 hover:underline">
+              Forgot your password?
+            </Link>
           </div>
           <Input
             id="password"
